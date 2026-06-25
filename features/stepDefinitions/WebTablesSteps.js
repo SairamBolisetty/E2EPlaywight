d1fd8422a377513422d8ea8dfcc97fa0738474f2
SairamBolisetty <Bolisetty.sairam01@gmail.com>
2026-06-25 23:25:01 +0530

    Added Assertions, new Employe to table

--- features/stepDefinitions/WebTablesSteps.js
+++ features/stepDefinitions/WebTablesSteps.js
@@ -6,5 +6,22 @@ Then('User performs actions on Web Tables', async function(){
     const webTablesPage = new WebTablesPage(this.page);
 
     await webTablesPage.webTablesButton.click();
+    await webTablesPage.addButton.click();
+    await webTablesPage.firstNameInput.fill('Dhanush');
+    await webTablesPage.lastNameInput.fill('B');
+    await webTablesPage.emailInput.fill('sai@gmail.com');
+    await webTablesPage.ageInput.fill('25');
+    await webTablesPage.salaryInput.fill('50000');
+    await webTablesPage.departmentInput.fill('IT');
+    await webTablesPage.submitButton.click();
+    await webTablesPage.searchBox.fill('Dhanush');
+    console.log(await webTablesPage.searchResultName.textContent());
+    expect(await webTablesPage.searchResultName.textContent()).toBe('Dhanush');
+    await webTablesPage.searchBox.clear();
+    await webTablesPage.editIcon.click();
+    await webTablesPage.salaryInput.clear();
+    await webTablesPage.salaryInput.fill('25000');
+    await webTablesPage.submitButton.click();
+    
 
 })
--- pageObjects/WebTablesPage.js
+++ pageObjects/WebTablesPage.js
@@ -3,7 +3,19 @@ class WebTablesPage extends BasePage {
     constructor(page) {
         super(page);
         this.webTablesButton = page.getByText('Web Tables');
-
+        this.addButton = page.getByRole('button', {name: 'Add'});
+        this.firstNameInput = page.getByPlaceholder('First Name');
+        this.lastNameInput = page.getByPlaceholder('Last Name');
+        this.emailInput = page.getByPlaceholder('name@example.com');
+        this.ageInput = page.getByPlaceholder('Age');
+        this.salaryInput = page.getByPlaceholder('Salary');
+        this.departmentInput = page.getByPlaceholder('Department');
+        this.submitButton = page.getByRole('button', {name: 'Submit'});
+        this.searchBox = page.getByPlaceholder('Type to search');
+        this.searchIcon = page.locator("//input[@placeholder='Type to search']//following::button[1]");
+        this.searchResultName = page.locator("//tr/td[1]")
+        this.editIcon = page.locator('tr').filter({ hasText: '50000' }).locator('[title="Edit"]');
+        
     }
 }
 module.exports = WebTablesPage;
